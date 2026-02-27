775fb0dbe408cb547a221c6fbe952237bc7920f4
Dong-Chen-1031 <dcdcdc1031@gmail.com>
2026-02-27 14:21:29 +0800

    更新 .gitignore 以包含 .env 檔案，並在 settings.py 中加入 DEV_MODE 的環境變數加載邏輯

--- settings.py
+++ settings.py
@@ -4,6 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DEV_MODE: bool = os.getenv("DEV_MODE", "False").lower() in ("true", "1", "t")
+
+if DEV_MODE:
+    load_dotenv(".env.dev", override=True)
+
 GROUP_ID: str = os.getenv("GROUP_ID", "")
 
 NOTION_TOKEN: str = os.getenv("NOTION_TOKEN", "")
@@ -12,10 +17,10 @@ GC_TOKEN_PATH: str = os.getenv("GC_TOKEN_PATH", "keys/token.json")
 
 GC_CREDENTIALS_PATH: str = os.getenv("GC_CREDENTIALS_PATH", "keys/credentials.json")
 
-LINE_DEVS_ID: list[str] = os.getenv("LINE_DEVS_ID", "").split()
+LINE_DEVS_ID: list[str] = os.getenv("LINE_DEVS_ID", "").split(",")
 
 PORT: int = int(os.getenv("PORT", "11111"))
 
-
 LINE_TOKEN: str = os.getenv("LINE_TOKEN", "")
+
 CHANNEL_SECRET: str = os.getenv("CHANNEL_SECRET", "")
